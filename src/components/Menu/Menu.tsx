d23f85a8f7dec5b53f5d33017b1b533f6d8d574e
Anit Nilay <anit.nilay20@gmail.com>
2020-10-20 11:47:41 +0530

    v0.1.0

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import { List } from '../..';
+import { ListProps } from '../List/List';
 
-export const Menu: React.FunctionComponent<React.DetailedHTMLProps<
-  React.HTMLAttributes<HTMLDivElement>,
-  HTMLDivElement
->> = (props) => {
-  return <List>{props.children}</List>;
+export const Menu: React.FC<ListProps> = (props) => {
+  return <List {...props}>{props.children}</List>;
 };
