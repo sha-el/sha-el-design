ca15b252ada34f47e1bc44e96b56824764ec3e53
Anit Nilay <anit.nilay20@gmail.com>
2020-10-30 00:16:24 +0530

    :sunglasses: Update Card Header Design
    * Expose Text Props.
    * Update row gutter to [0, 0].

--- src/components/Card/CardHeader.tsx
+++ src/components/Card/CardHeader.tsx
@@ -4,9 +4,10 @@ import { Row, Col } from '../../';
 import { lightText } from '../../helpers/color';
 import { ThemeConsumer } from '../Theme/Theme';
 import { Text } from '../Text';
+import { TextProps } from '../Text/Text';
 
 export const CardHeader: React.FunctionComponent<CardHeaderProps> = (props) => {
-  const { children, subtitle, className, action, ...restProps } = props;
+  const { children, subtitle, className, action, headerTextProps, subtitleTextProps, ...restProps } = props;
 
   return (
     <ThemeConsumer>
@@ -16,12 +17,14 @@ export const CardHeader: React.FunctionComponent<CardHeaderProps> = (props) => {
         });
         return (
           <div className={classes(css, className)} {...restProps}>
-            <Row alignItems="center">
+            <Row gutter={[0, 0]} alignItems="center">
               <Col style={{ paddingLeft: '0' }} flex="1 0 auto">
-                <Text margin="0" variant="h5">
+                <Text margin="0" variant="h5" {...headerTextProps}>
                   {children}
                 </Text>
-                <Text color={lightText(theme)}>{subtitle}</Text>
+                <Text color={lightText(theme)} {...subtitleTextProps}>
+                  {subtitle}
+                </Text>
               </Col>
               <Col flex="0 1 auto">{action}</Col>
             </Row>
@@ -35,4 +38,6 @@ export const CardHeader: React.FunctionComponent<CardHeaderProps> = (props) => {
 export interface CardHeaderProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   subtitle?: React.ReactNode;
   action?: React.ReactNode;
+  headerTextProps?: TextProps;
+  subtitleTextProps?: TextProps;
 }
