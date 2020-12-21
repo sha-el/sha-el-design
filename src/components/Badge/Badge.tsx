7148749777096222d51858eef4a2abb11c828c04
Prateek Shourya <prateekshourya29@gmail.com>
2020-12-21 22:55:48 +0530

    Move badge style to new file

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,38 +1,10 @@
 import * as React from 'react';
-import { createUseStyles } from 'react-jss';
-import { getColor } from '../../helpers';
+import { style } from './style';
 
 export const Badge: React.FunctionComponent<BadgeProps> = (props) => {
   const count = props.maxCount && props.maxCount < props.count ? `${props.maxCount}+` : props.count;
-  const style = createUseStyles({
-    container: {
-      position: 'relative',
-      boxSizing: 'border-box',
-      display: 'inline-block',
-    },
-    count: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'absolute',
-      top: '0',
-      right: '0',
-      transform: 'translate(50%, -50%)',
-      background: props.color,
-      color: getColor(props.color),
-      borderRadius: '10px',
-      whiteSpace: 'nowrap',
-      fontSize: '12px',
-      lineHeight: '20px',
-      minWidth: '20px',
-      height: '20px',
-      fontWeight: 500,
-      padding: `0 ${count ? String(count).length * 2 : 0}px`,
-      zIndex: 1,
-    },
-  });
+  const css = style(props);
 
-  const css = style();
   return (
     <div className={css.container}>
       <sup className={css.count}>{count}</sup>
