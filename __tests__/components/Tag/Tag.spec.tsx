9cdf4c274ff4e118637f96b4ae6441577293dc23
Prateek Shourya <prateekshourya29@gmail.com>
2020-12-22 20:21:59 +0530

    Tag test case (#44)
    **Replace typestyle with jss in tag**
    **Move tag style function to separate file**
    **Add tag test case**

---
+++ __tests__/components/Tag/Tag.spec.tsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { Tag } from '../../../src';
+
+import '@testing-library/jest-dom';
+
+let container: HTMLDivElement;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe('Tag', () => {
+  const fn = jest.fn();
+
+  it('should render a default tag with children', () => {
+    act(() => {
+      ReactDOM.render(
+        <Tag color="red" onClick={fn}>
+          Basic
+        </Tag>,
+        container,
+      );
+    });
+
+    const tag = document.querySelector('span');
+
+    tag.click();
+    expect(fn).toBeCalledTimes(1);
+
+    expect(tag.innerHTML).toBe('Basic');
+    expect(tag).toHaveStyle(`
+    color: rgb(255, 255, 255);
+    height: 36px;
+    margin: 5px;
+    display: inline-flex;
+    padding: 0px 20px;
+    font-size: 0.8125rem;
+    min-width: 64px;
+    background: red;
+    box-shadow: 0 3px 1px -2px #E8EAFC,
+      0 2px 2px 0 #B2B2B21A,
+      0 1px 5px 0 #9A9A9A1A;
+    box-sizing: border-box;
+    text-align: center;
+    align-items: center;
+    font-weight: 500;
+    line-height: 36px;
+    border-radius: 4px;
+    letter-spacing: 0.02857em;
+    text-transform: uppercase;
+    `);
+  });
+
+  it('should change text color', () => {
+    act(() => {
+      ReactDOM.render(
+        <Tag color="black" textColor="red">
+          Blue Text
+        </Tag>,
+        container,
+      );
+
+      const tag = document.querySelector('span');
+
+      expect(tag).toHaveStyle(`
+        color: red;
+      `);
+    });
+  });
+
+  it('should add custom styles', () => {
+    act(() => {
+      ReactDOM.render(
+        <Tag color="red" style={{ padding: '10px' }}>
+          Style
+        </Tag>,
+        container,
+      );
+    });
+
+    const tag = document.querySelector('span');
+
+    expect(tag).toHaveStyle(`
+      padding: 10px;
+    `);
+  });
+
+  it('should render an outline tag', () => {
+    act(() => {
+      ReactDOM.render(
+        <Tag color="red" outline>
+          Outline
+        </Tag>,
+        container,
+      );
+
+      const tag = document.querySelector('span');
+
+      expect(tag).toHaveStyle(`
+        border: 1px solid red;
+        color: red;
+      `);
+    });
+  });
+
+  it('should render a chip tag', () => {
+    act(() => {
+      ReactDOM.render(
+        <Tag color="red" chips>
+          Chip
+        </Tag>,
+        container,
+      );
+    });
+
+    const chip = document.querySelector('svg');
+
+    expect(chip).not.toBeNull();
+  });
+
+  it('should render a small tag', () => {
+    act(() => {
+      ReactDOM.render(
+        <Tag color="red" size="SMALL">
+          Small
+        </Tag>,
+        container,
+      );
+    });
+
+    const tag = document.querySelector('span');
+
+    expect(tag).toHaveStyle(`
+    padding: 0 14px;
+    minWidth: 50px;
+    lineHeight: 22px;
+    height: 22px;
+    fontSize: 0.7125rem;
+    `);
+  });
+});
---
+++ src/components/Tag/style.ts
@@ -0,0 +1,67 @@
+import { createUseStyles } from 'react-jss';
+import { getColor } from '../../helpers';
+import { shadow } from '../../helpers/style';
+import { TagProps } from './Tag';
+
+const borderStyle = (props: TagProps) => {
+  if (!props.outline) {
+    return {
+      background: props.color,
+      color: props.textColor || getColor(props.color),
+    };
+  }
+
+  return {
+    border: '1px solid ' + props.color,
+    color: props.textColor || props.color,
+  };
+};
+
+const sizeCss = (size: TagProps['size']) => {
+  if (size === 'DEFAULT') {
+    return {
+      padding: '0 20px',
+      minWidth: '64px',
+      lineHeight: '36px',
+      height: '36px',
+      fontSize: '0.8125rem',
+    };
+  }
+
+  return {
+    padding: '0 14px',
+    minWidth: '50px',
+    lineHeight: '22px',
+    height: '22px',
+    fontSize: '0.7125rem',
+  };
+};
+
+export const chipIconCss = createUseStyles({
+  chipIcon: {
+    marginLeft: '5px',
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export const style = createUseStyles({
+  tag: ({ props, theme }) => {
+    return {
+      ...borderStyle(props),
+      ...sizeCss(props.size),
+      fontWeight: 500,
+      margin: '5px',
+      display: 'inline-flex',
+      boxShadow: shadow('DEFAULT', theme),
+      cursor: props.onClick && 'pointer',
+      textTransform: 'uppercase',
+      textAlign: 'center',
+      borderRadius: props.chips ? '16px' : '4px',
+      letterSpacing: '0.02857em',
+      boxSizing: 'border-box',
+      alignItems: 'center',
+    };
+  },
+});
