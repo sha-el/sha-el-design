6382ab4f7bbbf53ec9cc5dd78b4e21be285d36ef
Prateek Shourya <prateekshourya29@gmail.com>
2020-12-30 14:47:11 +0530

    CheckBox test case (#46)
    * CheckBox test case
    
    **Replace typestyle with jss in checkbox**
    **Move checkbox style function to separate file**
    **Add checkbox test case**
    
    * Fix bug input element not disable

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { stylesheet } from 'typestyle';
-import { ThemeConsumer, Theme } from '../Theme/Theme';
 import { Text } from '../Text';
 import { MdCheckBox, MdCheckBoxOutlineBlank, MdIndeterminateCheckBox } from 'react-icons/md';
 import { Col, Row } from '../Grid';
-import { disabledColor, lightText } from '../../helpers/color';
+import { disabledColor } from '../../helpers/color';
+import { Theme, useTheme } from '../Theme/Theme';
+import { style } from './style';
 
 export const CheckBox: React.FunctionComponent<CheckBoxProps> = (props) => {
   const { label, intermediate, onChange, color, disabled, ...rest } = props;
@@ -14,67 +14,42 @@ export const CheckBox: React.FunctionComponent<CheckBoxProps> = (props) => {
   const input = React.createRef<HTMLInputElement>();
 
   const onContainerClick = () => {
-    if (input.current) {
-      input.current.click();
-    }
+    input.current?.click();
   };
 
+  const theme = useTheme();
+  const css = style({ theme, checked, color, disabled });
+
   return (
-    <ThemeConsumer>
-      {(theme) => {
-        const css = style(theme, checked, color, disabled);
-
-        return (
-          <Row
-            alignItems="center"
-            gutter={[0, 0]}
-            className={css.container}
-            onClick={() => !disabled && onContainerClick()}
-          >
-            <input
-              className={css.input}
-              ref={input}
-              type="checkbox"
-              checked={checked}
-              onChange={(e) => {
-                onChange?.(e);
-                updateChecked(e.target.checked);
-              }}
-              {...rest}
-            />
-            <Col flex="0 1 auto">
-              {intermediate ? (
-                <MdIndeterminateCheckBox size="22px" className={css.svg} />
-              ) : checked ? (
-                <MdCheckBox size="22px" className={css.svg} />
-              ) : (
-                <MdCheckBoxOutlineBlank size="22px" className={css.svg} />
-              )}
-            </Col>
-            <Col flex="1 0 auto">
-              <Text color={disabled && disabledColor(theme)} fontSize="1rem" margin="0 0 0 9px">
-                {label}
-              </Text>
-            </Col>
-          </Row>
-        );
-      }}
-    </ThemeConsumer>
+    <Row alignItems="center" gutter={[0, 0]} className={css.container} onClick={() => !disabled && onContainerClick()}>
+      <input
+        className={css.input}
+        ref={input}
+        type="checkbox"
+        checked={checked}
+        disabled={disabled}
+        onChange={(e) => {
+          onChange?.(e);
+          updateChecked(e.target.checked);
+        }}
+        {...rest}
+      />
+      <Col flex="0 1 auto">
+        {intermediate ? (
+          <MdIndeterminateCheckBox size="22px" className={css.svg} />
+        ) : checked ? (
+          <MdCheckBox size="22px" className={css.svg} />
+        ) : (
+          <MdCheckBoxOutlineBlank size="22px" className={css.svg} />
+        )}
+      </Col>
+      <Col flex="1 0 auto">
+        <Text color={disabled && disabledColor(theme)} fontSize="1rem" margin="0 0 0 9px">
+          {label}
+        </Text>
+      </Col>
+    </Row>
   );
-};
-
-const style = (theme: Theme, checked: boolean, color: CheckBoxProps['color'], disabled: boolean) => {
-  return stylesheet({
-    container: {
-      cursor: disabled ? 'not-allowed' : 'pointer',
-    },
-    input: {
-      display: 'none',
-    },
-    svg: {
-      fill: disabled ? disabledColor(theme) : checked ? theme[color] || color : lightText(theme),
-    },
-  });
 };
 
 export interface CheckBoxProps
