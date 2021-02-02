55f534b4c292fd57dc7319341fb262018b613e9f
Prateek Shourya <prateekshourya29@gmail.com>
2021-02-02 22:59:45 +0530

    Add test for divider

---
+++ __tests__/components/Divider/Divider.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { Divider } from '../../../src';
+
+describe('Divider', () => {
+  it('should render a divider', () => {
+    render(<Divider className="divider" />);
+
+    const divider = document.querySelector('.divider');
+
+    expect(divider).toHaveStyle(`
+      width: 100%;
+      margin: 5px 0px;
+      display: inline-block;
+      z-index: 2;
+      position: relative;
+      background: rgb(255, 255, 255);
+      text-align: center;
+    `);
+  });
+
+  it('should render a divider with children', () => {
+    render(<Divider className="divider">Hello</Divider>);
+
+    const divider = document.querySelector('.divider');
+
+    expect(divider.innerHTML).toBe('Hello');
+  });
+});
