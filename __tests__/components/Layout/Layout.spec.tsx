49c79bb857243a80de7a6b01389c131f7e790766
Anit Nilay <anit.nilay20@gmail.com>
2022-01-16 18:28:45 +0530

    Fix test

--- __tests__/components/Layout/Layout.spec.tsx
+++ __tests__/components/Layout/Layout.spec.tsx
@@ -111,8 +111,8 @@ describe('Layout', () => {
       min-width: 0;
       flex: 1 0 auto;
       min-height: 100%;
-      width: calc(100% - 115px);
-      margin: 5px 5px 5px 123px;
+      width: calc(100% - 95px);
+      margin: 5px 5px 5px 103px;
     `);
   });
 
