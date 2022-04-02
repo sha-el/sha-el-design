c0105238574c7e9381d59d67d58ddb093eb6cec4
Anit Nilay <anit.nilay20@gmail.com>
2022-04-02 13:33:04 +0530

    v0.2.14

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -9,7 +9,13 @@ import { Input } from '../Input';
 export const Pagination: React.FC<PaginationProps> = (props) => {
   const { totalCount, batchSize, currentPage, onChange, itemsPerPage, justify } = props;
   return (
-    <Row alignItems="center" justifyContent={justify} gutter={[20, 0]} className="sha-el-pagination">
+    <Row
+      style={{ maxWidth: '100%' }}
+      alignItems="center"
+      justifyContent={justify}
+      gutter={10}
+      className="sha-el-pagination"
+    >
       {itemsPerPage && (
         <Col flex="0 1 auto">
           <Menu
